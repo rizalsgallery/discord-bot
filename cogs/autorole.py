86b81c9acfc6bccc61611f9de6aedb31fe8a3d7f
rizalsgallery <rizalsgallery@gmail.com>
2026-05-09 13:21:05 +0200

    Update autorole.py

--- cogs/autorole.py
+++ cogs/autorole.py
@@ -1,22 +1,22 @@
 import discord
 from discord.ext import commands
   
- AUTO_ROLE_ID = 1499866593084178434
+AUTO_ROLE_ID = 1499866593084178434
   
   
- class AutoRole(commands.Cog):
-     def __init__(self, bot):
+class AutoRole(commands.Cog):
+    def __init__(self, bot):
           self.bot = bot
   
-     @commands.Cog.listener()
-     async def on_member_join(self, member):
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
   
-          role = member.guild.get_role(AUTO_ROLE_ID)
+        role = member.guild.get_role(AUTO_ROLE_ID)
   
-          if role:
-              try:
-                  await member.add_roles(role)
-              except Exception as e:
+        if role:
+            try:
+                await member.add_roles(role)
+            except Exception as e:
                   print(e)
   
 
