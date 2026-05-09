cf59c8a535d48ea1570f70cb343734c62009fc02
rizalsgallery <rizalsgallery@gmail.com>
2026-05-09 12:32:31 +0200

    Create autorole.py

---
+++ cogs/autorole.py
@@ -0,0 +1,24 @@
+  import discord
+  from discord.ext import commands
+  
+  AUTO_ROLE_ID = 1499866593084178434
+  
+  
+  class AutoRole(commands.Cog):
+      def __init__(self, bot):
+          self.bot = bot
+  
+      @commands.Cog.listener()
+      async def on_member_join(self, member):
+  
+          role = member.guild.get_role(AUTO_ROLE_ID)
+  
+          if role:
+              try:
+                  await member.add_roles(role)
+              except Exception as e:
+                  print(e)
+  
+
+async def setup(bot):
+    await bot.add_cog(AutoRole(bot))
